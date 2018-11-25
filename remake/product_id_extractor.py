5aaed18b16b03a6db1834c0f999ce39b228f27ec
EL-S <31227083+EL-S@users.noreply.github.com>
2018-11-26 00:00:04 +1100

    Collects Product IDs
    Fast

--- remake/product_id_extractor.py
+++ remake/product_id_extractor.py
@@ -10,13 +10,22 @@ for line in robots_text:
     if "Sitemap:" in line and "Products" in line:
         product_sitemap_url = line.split("Sitemap:")[1].strip()
 
+print(product_sitemap_url)
 product_sitemap = requests.get(product_sitemap_url, headers=headers).text
 #print(product_sitemap)
+print(len('你'.encode('utf-8')))
+print(len(product_sitemap.encode('utf-8')))
 
 with open("product_sitemap.xml", "w") as file:
     file.write(product_sitemap)
 
 root = ET.fromstring(product_sitemap)
 
+product_ids = []
+
 for child in root:
-    print(child.tag, child.attrib)
+    if "product" in child[0].text:
+        product_id = child[0].text.split("/")[-2]
+        product_ids.append(product_id)
+
+print(product_ids, len(product_ids))
